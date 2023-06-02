9e4c3cbc1ea2d7bc5521b8f9c7af6363802ec90b
Vagner Castro <vcastro@xprt.io>
2023-06-02 13:58:53 -0300

    Add integration with Auth0

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,25 @@
-exports.handler = async () => {
-  const mySecret = process.env.MY_SECRET;
+const { wrap } = require("@netlify/integrations");
+const { withAuth0 } = require("@netlify/auth0");
 
-  return {
-    statusCode: 200,
-    body: `hello world! I have a ${mySecret}`,
-  };
-};
+const withIntegrations = wrap(withAuth0);
+
+console.log("AUTH0_DOMAIN", process.env.AUTH0_DOMAIN);
+console.log("AUTH0_CLIENT_ID", process.env.AUTH0_CLIENT_ID);
+console.log("AUTH0_ISSUER", process.env.AUTH0_ISSUER);
+console.log("AUTH0_AUDIENCE", process.env.AUTH0_AUDIENCE);
+
+exports.handler = withIntegrations(
+  async () => {
+    const mySecret = process.env.AUTH0_DOMAIN;
+
+    return {
+      statusCode: 200,
+      body: `hello world! I have a ${mySecret}`,
+    };
+  },
+  {
+    auth0: {
+      required: true,
+    },
+  }
+);
